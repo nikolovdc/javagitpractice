9357dd8ce1ebf96c54ab6bf0233c65c3f7bd496a
nikolovdc <nikolovdc@appstate.edu>
2023-01-27 18:26:32 -0500

    created Demo

---
+++ Demo.java
@@ -0,0 +1,15 @@
+pubic class Demo {
+
+	public static void main (String args[])
+	{
+		Cat cat = new Cat("Mr Kitty");
+		cat.makeNoise();
+		cat.eat();
+
+		Dog dog = new Dog("Ms. Puppy");
+		dog.makeNoise();
+		dog.eat();
+		
+	}
+
+}
