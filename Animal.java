c75aa35b3b8785fcecfa0f1b3e7485f1f0456692
nikolovdc <nikolovdc@appstate.edu>
2023-01-27 17:53:05 -0500

    created abstract class

--- Animal.java
+++ Animal.java
@@ -1,5 +1,17 @@
-public class Animal {
+abstract public class Animal {
+	
+	public String name;
+ 	
+	public Animal(String name) {
+		this.name = name;
+		
+	}
 
+	public abstract void makeNoise();
+
+	public void eat() {
+		System.out.println("Eating... mmm");
+	}
 
 
 
