c50813b490e025c61c89a16ddc01ac093465f766
nikolovdc <nikolovdc@appstate.edu>
2023-01-27 19:19:55 -0500

    Updated Demo

--- Demo.java
+++ Demo.java
@@ -2,22 +2,28 @@ public class Demo {
 
 	public static void main (String args[])
 	{
+		
+		System.out.println("Here's a cat!");
 		Cat cat = new Cat("Mr Kitty");
 		cat.makeNoise();
-		cat.eat();
-
+		System.out.println(" ");
+		
+		System.out.println("And here's a dog!");	
 		Dog dog = new Dog("Ms. Puppy");
 		dog.makeNoise();
-		dog.eat();
+		System.out.println(" ");
 		
 		// polymorphic variable
 		Animal animal;
-	
+		
+		
 		// cat
-		animal = new Cat("Mr Meow Meow");
+		System.out.println("Time for Mr Kitty to eat!");
+		animal = new Cat("Mr Kitty");
 		animal.eat();
 
 		// dog 
+		System.out.println("Time for Ms Ruff Ruff to eat as well!");
 		animal = new Dog("Ms Ruff Ruff");
 		animal.eat();		
 	}
